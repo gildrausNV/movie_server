39c0d144c2a3e72e852f4cd524107254a6b3bc1f
gildrausNV <nikola.vujcic.001@gmail.com>
2024-04-09 22:19:31 +0200

    Changed securityConfiguration for /users GET and /reviews POST endpoints

--- src/main/java/com/example/movie_server/config/SecurityConfiguration.java
+++ src/main/java/com/example/movie_server/config/SecurityConfiguration.java
@@ -22,33 +22,23 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .cors().and()
-//                .csrf().disable()
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/auth/**").permitAll()
-//                        .requestMatchers("/movies**").hasAnyAuthority(Role.USER.getAuthority())
-//                        .requestMatchers("/actors**").hasAnyAuthority(Role.USER.getAuthority())
                         .requestMatchers(HttpMethod.GET, "/movies**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/movies/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/actors**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/actors/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/users**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/comments**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/reviews**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/users**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/movies**").hasAnyAuthority(Role.ADMIN.getAuthority())
                         .requestMatchers(HttpMethod.POST, "/actors**").hasAnyAuthority(Role.ADMIN.getAuthority())
-                        .requestMatchers(HttpMethod.POST, "/users**").hasAnyAuthority(Role.USER.getAuthority())
-                        .requestMatchers(HttpMethod.POST, "/comments**").hasAnyAuthority(Role.USER.getAuthority())
-                        .requestMatchers(HttpMethod.DELETE, "/comments**").hasAnyAuthority(Role.USER.getAuthority())
                         .requestMatchers(HttpMethod.POST, "/reviews**").hasAnyAuthority(Role.USER.getAuthority())
                         .requestMatchers(HttpMethod.DELETE, "/reviews**").hasAnyAuthority(Role.USER.getAuthority())
                         .requestMatchers(HttpMethod.POST, "/reviews**").hasAnyAuthority(Role.ADMIN.getAuthority())
 
                         .anyRequest().authenticated())
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
